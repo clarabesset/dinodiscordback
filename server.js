b708ca82aa68d71ce11706d0578c0dc1ec15d353
Camille Charpentier <cam.charpentier@live.fr>
2019-07-24 09:20:34 +0200

    reinitize board when user disconnects

--- server.js
+++ server.js
@@ -60,9 +60,6 @@ let players = [];
 io.of("/room").on("connection", function(socket) {
   connectedPlayers += 1;
 
-  console.log(connectedPlayers);
-  socket.emit("aloha");
-
   socket.on("player-join", color => {
     if (players.length < 2) players.push({ color, nb: players.length + 1 });
 
@@ -71,23 +68,6 @@ io.of("/room").on("connection", function(socket) {
 
     socket.emit("confirm-player-join", players);
   });
-
-  socket.on("message", data => {
-    console.log(data);
-    socket.broadcast.emit("message", data);
-  });
-  if (connectedPlayers < 4) {
-    socket.broadcast.emit(
-      "message",
-      `There are ${4 - connectedPlayers} left to go !`
-    );
-    socket.emit("message", `There are ${4 - connectedPlayers} left to go !`);
-  } else {
-    io.of("room").emit("message", "the tournament will start soon");
-    socket.emit("message", "the tournament will start soon");
-  }
-  socket.on("disconnect", () => (connectedPlayers -= 1));
-  //setInterval(() => socket.emit("message", "hello"), 2000)
 });
 server.listen(process.env.PORT, () => {
   console.log("App hosted on: ", process.env.SITE_URL);
